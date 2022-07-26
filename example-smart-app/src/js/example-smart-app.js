90c24cb2e10a01e11f80dac7f3f5e519e7a638f1
kevinroutley <109542435+kevinroutley@users.noreply.github.com>
2022-07-26 13:42:54 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -42,7 +42,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var tempature = byCodes('8310-5');
+          var temperature = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -50,7 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.temp = getQuantityValueAndUnit(tempature[0]);
+          p.temperature = getQuantityValueAndUnit(temperature[0]);
           
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -86,7 +86,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      tempature: {value: ''},
+      temperature: {value: ''},
     };
   }
 
@@ -130,7 +130,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    $('#tempature').html(p.temp);
+    $('#temperature').html(p.temperature);
   };
 
 })(window);
