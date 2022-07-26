93c0d9aec25cfe525d8e985e9c3a92725b6b1ff2
kevinroutley <109542435+kevinroutley@users.noreply.github.com>
2022-07-26 11:35:37 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -38,8 +38,8 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'http://loinc.org|8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'http://loinc.org|8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
